895d43674e592a28c4044c7a378f5536c7042e0d
Emmanuel BALLERY <emmanuel.ballery@gmail.com>
2015-01-15 00:13:30 +0100

    Allow encoding configuration

--- Converter/StringConverter.php
+++ Converter/StringConverter.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace EB\DoctrineBundle\Converter;
+
 use EB\DoctrineBundle\Entity\LoggableInterface;
 
 /**
@@ -10,8 +11,6 @@ use EB\DoctrineBundle\Entity\LoggableInterface;
  */
 class StringConverter
 {
-    const ENC = 'utf-8';
-
     /**
      * @var array
      */
@@ -28,17 +27,24 @@ class StringConverter
     private $accValues;
 
     /**
+     * @var string
+     */
+    private $encoding;
+
+    /**
      * Load accent values
      *
-     * @param array $acc       Array of accent => no accent
-     * @param array $accKeys   Keys of first array
-     * @param array $accValues Values of first array
+     * @param array  $acc       Array of accent => no accent
+     * @param array  $accKeys   Keys of first array
+     * @param array  $accValues Values of first array
+     * @param string $encoding  Encoding
      */
-    public function __construct(array $acc, array $accKeys, array $accValues)
+    public function __construct(array $acc, array $accKeys, array $accValues, $encoding = 'utf-8')
     {
         $this->acc = $acc;
         $this->accKeys = $accKeys;
         $this->accValues = $accValues;
+        $this->encoding = $encoding;
     }
 
     /**
@@ -81,6 +87,26 @@ class StringConverter
     }
 
     /**
+     * Camelize
+     *
+     * @param string $string
+     *
+     * @return string
+     */
+    public function camelize($string)
+    {
+        $string = strip_tags($string);
+        $string = mb_strtolower($string, $this->encoding);
+        $string = str_replace($this->accKeys, $this->accValues, $string);
+        $string = mb_strtolower($string, $this->encoding);
+        $string = preg_replace('/[^a-z0-9\.-]+/', ' ', $string);
+        $string = mb_convert_case($string, MB_CASE_TITLE, $this->encoding);
+        $string = str_replace(' ', '', $string);
+
+        return trim(mb_strtolower(mb_substr($string, 0, 1, $this->encoding), $this->encoding) . mb_substr($string, 1, null, $this->encoding));
+    }
+
+    /**
      * Cut string
      *
      * @param string $string  String to cut
@@ -93,120 +119,24 @@ class StringConverter
     {
         // Secure & cut
         $string = strip_tags($string);
-        $newString = mb_substr($string, 0, $length, self::ENC);
+        $newString = mb_substr($string, 0, $length, $this->encoding);
 
         // Is this string longer ?
-        if ($length >= mb_strlen($string, self::ENC)) {
+        if ($length >= mb_strlen($string, $this->encoding)) {
             return $newString;
         }
 
         // Cut near last space
-        if (false !== $lastSpacePos = mb_strrpos($newString, ' ', self::ENC)) {
-            $newString = trim(mb_substr($newString, 0, $lastSpacePos + 1, self::ENC));
+        if (false !== $lastSpacePos = mb_strrpos($newString, ' ', $this->encoding)) {
+            $newString = trim(mb_substr($newString, 0, $lastSpacePos + 1, $this->encoding));
         }
 
         // No ending if sentence finises with a dot
-        if ('.' !== mb_substr($newString, mb_strlen($newString, self::ENC) - 1, 1, self::ENC)) {
+        if ('.' !== mb_substr($newString, mb_strlen($newString, $this->encoding) - 1, 1, $this->encoding)) {
             $newString .= $endLine;
         }
 
         return $newString;
-    }
-
-    /**
-     * Slug
-     *
-     * @param string $string
-     *
-     * @return string
-     */
-    public function slug($string)
-    {
-        return $this->uri($string);
-    }
-
-    /**
-     * URI
-     *
-     * @param string $string
-     *
-     * @return string
-     */
-    public function uri($string)
-    {
-        $string = strip_tags($string);
-        $string = mb_strtolower($string, self::ENC);
-        $string = str_replace($this->accKeys, $this->accValues, $string);
-        $string = trim($string);
-        $string = preg_replace('/[^a-z0-9-]+/', '-', $string);
-        $string = preg_replace('`(-){2,}`', '-', $string);
-
-        return trim($string, '-');
-    }
-
-    /**
-     * Search
-     *
-     * @param string $string
-     *
-     * @return string
-     */
-    public function search($string)
-    {
-        $string = strip_tags($string);
-        $string = mb_strtolower($string, self::ENC);
-        $string = str_replace($this->accKeys, $this->accValues, $string);
-        $string = str_replace('-', ' ', $string);
-        $string = trim($string);
-        $string = preg_replace('/[^a-z0-9\.-]+/', ' ', $string);
-        $string = preg_replace('`( ){2,}`', ' ', $string);
-        $string = trim($string);
-
-        $words = array_unique(explode(' ', $string));
-        $words = array_filter(array_map(function ($word) {
-            return mb_strlen($word, self::ENC) > 2 ? $word : null;
-        }, $words));
-
-        return implode(' ', $words);
-    }
-
-    /**
-     * Camelize
-     *
-     * @param string $string
-     *
-     * @return string
-     */
-    public function camelize($string)
-    {
-        $string = strip_tags($string);
-        $string = mb_strtolower($string, self::ENC);
-        $string = str_replace($this->accKeys, $this->accValues, $string);
-        $string = mb_strtolower($string, self::ENC);
-        $string = preg_replace('/[^a-z0-9\.-]+/', ' ', $string);
-        $string = mb_convert_case($string, MB_CASE_TITLE, self::ENC);
-        $string = str_replace(' ', '', $string);
-
-        return trim(mb_strtolower(mb_substr($string, 0, 1, self::ENC), self::ENC) . mb_substr($string, 1, null, self::ENC));
-    }
-
-    /**
-     * Underscore
-     *
-     * @param string $string
-     *
-     * @return string
-     */
-    public function underscore($string)
-    {
-        $string = strip_tags($string);
-        $string = mb_strtolower($string, self::ENC);
-        $string = str_replace($this->accKeys, $this->accValues, $string);
-        $string = mb_strtolower($string, self::ENC);
-        $string = preg_replace('/[^a-z0-9\.-]+/', ' ', $string);
-        $string = str_replace(' ', '_', $string);
-
-        return trim($string);
     }
 
     /**
@@ -234,11 +164,11 @@ class StringConverter
             return $value->format('d/m/Y H:i:s');
         }
         if ($value instanceof LoggableInterface) {
-            return (string) $value;
+            return (string)$value;
         }
         if (is_object($value)) {
             if (method_exists($value, '__toString')) {
-                return (string) $value;
+                return (string)$value;
             }
             if (method_exists($value, 'getId')) {
                 return sprintf('%s[%u]', get_class($value), call_user_func([$value, 'getId']));
@@ -251,5 +181,81 @@ class StringConverter
         }
 
         return '?';
+    }
+
+    /**
+     * Search
+     *
+     * @param string $string
+     *
+     * @return string
+     */
+    public function search($string)
+    {
+        $string = strip_tags($string);
+        $string = mb_strtolower($string, $this->encoding);
+        $string = str_replace($this->accKeys, $this->accValues, $string);
+        $string = str_replace('-', ' ', $string);
+        $string = trim($string);
+        $string = preg_replace('/[^a-z0-9\.-]+/', ' ', $string);
+        $string = preg_replace('`( ){2,}`', ' ', $string);
+        $string = trim($string);
+
+        $words = array_unique(explode(' ', $string));
+        $words = array_filter(array_map(function ($word) {
+            return mb_strlen($word, $this->encoding) > 2 ? $word : null;
+        }, $words));
+
+        return implode(' ', $words);
+    }
+
+    /**
+     * Slug
+     *
+     * @param string $string
+     *
+     * @return string
+     */
+    public function slug($string)
+    {
+        return $this->uri($string);
+    }
+
+    /**
+     * Underscore
+     *
+     * @param string $string
+     *
+     * @return string
+     */
+    public function underscore($string)
+    {
+        $string = strip_tags($string);
+        $string = mb_strtolower($string, $this->encoding);
+        $string = str_replace($this->accKeys, $this->accValues, $string);
+        $string = mb_strtolower($string, $this->encoding);
+        $string = preg_replace('/[^a-z0-9\.-]+/', ' ', $string);
+        $string = str_replace(' ', '_', $string);
+
+        return trim($string);
+    }
+
+    /**
+     * URI
+     *
+     * @param string $string
+     *
+     * @return string
+     */
+    public function uri($string)
+    {
+        $string = strip_tags($string);
+        $string = mb_strtolower($string, $this->encoding);
+        $string = str_replace($this->accKeys, $this->accValues, $string);
+        $string = trim($string);
+        $string = preg_replace('/[^a-z0-9-]+/', '-', $string);
+        $string = preg_replace('`(-){2,}`', '-', $string);
+
+        return trim($string, '-');
     }
 }
